68c046ca6a92dab6ed5d39f6683c965d98caa78d
SinaverFrontend <141609479+SinaverFrontend@users.noreply.github.com>
2023-09-13 18:52:52 +0300

    fix: changed logic which show style-switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import About from './components/About';
 import Home from './components/Home';
 import Switcher from './components/Switcher';
@@ -11,36 +10,13 @@ import './i18n';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-    useEffect(() => {
-        const dayNight = document.querySelector('.day-night');
-
-        if (dayNight) {
-            dayNight.addEventListener('click', () => {
-                dayNight.querySelector('i')?.classList.toggle('fa-sun');
-                dayNight.querySelector('i')?.classList.toggle('fa-moon');
-                document.body.classList.toggle('dark');
-            });
-
-            window.addEventListener('load', () => {
-                if (document.body.classList.contains('dark')) {
-                    dayNight.querySelector('i')?.classList.add('fa-sun');
-                } else {
-                    dayNight.querySelector('i')?.classList.add('fa-moon');
-                }
-            });
-        }
-        return () => {
-            window.removeEventListener('load', () => {});
-        };
-    }, []);
-
     return (
         <BrowserRouter>
-            <div className="flex flex-row">
+            <div className="flex h-screen">
                 <div className="flex flex-col">
                     <Navbar />
                 </div>
-                <div className="flex flex-col pl-52">
+                <div className="flex flex-col px-52">
                     <Routes>
                         <Route path="/" Component={Home} />
                         <Route path="/about" Component={About} />
@@ -50,7 +26,7 @@ function App() {
                     </Routes>
                 </div>
             </div>
-            {/* <Switcher /> */}
+            <Switcher />
         </BrowserRouter>
     );
 }
--- src/components/Switcher.tsx
+++ src/components/Switcher.tsx
@@ -1,54 +1,95 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Switcher() {
-    const alternateStyles = document.querySelectorAll('.alternate-style');
+    const [dark, setDark] = useState(false);
+    const [colors, setColors] = useState(false);
 
-    function setActiveStyle(color: string) {
-        alternateStyles.forEach(style => {
-            if (color === style.getAttribute('title')) {
-                style.removeAttribute('disabled');
-            } else {
-                style.setAttribute('disabled', 'true');
-            }
-        });
+    function changeDark() {
+        const isDark = document.body.classList.contains('dark');
+
+        if (isDark) {
+            document.body.classList.remove('dark');
+        } else {
+            document.body.classList.add('dark');
+        }
+
+        setDark(prev => !prev);
     }
 
-    useEffect(() => {
-        window.addEventListener('scroll', () => {
-            if (document.querySelector('.style-switcher')?.classList.contains('open')) {
-                document.querySelector('.style-switcher')?.classList.remove('open');
-            }
-        });
-    }, []);
+    function changeColors() {
+        setColors(prev => !prev);
+    }
+    // useEffect(() => {
+    //     const dayNight = document.querySelector('.day-night');
 
-    useEffect(() => {
-        const styleSwitcherToggle = document.querySelector('.style-switcher-toggler');
-        if (styleSwitcherToggle)
-            styleSwitcherToggle.addEventListener('click', () => {
-                document.querySelector('.style-switcher')?.classList.toggle('open');
-            });
-    }, []);
+    //     if (dayNight) {
+    //         dayNight.addEventListener('click', () => {
+    //             dayNight.querySelector('i')?.classList.toggle('fa-sun');
+    //             dayNight.querySelector('i')?.classList.toggle('fa-moon');
+    //             document.body.classList.toggle('dark');
+    //         });
 
-    const styleSwitcherToggle = document.querySelector('.style-switcher-toggler');
-    styleSwitcherToggle?.addEventListener('click', () => {
-        document.querySelector('.style-switcher')?.classList.toggle('open');
-    });
+    //         window.addEventListener('load', () => {
+    //             if (document.body.classList.contains('dark')) {
+    //                 dayNight.querySelector('i')?.classList.add('fa-sun');
+    //             } else {
+    //                 dayNight.querySelector('i')?.classList.add('fa-moon');
+    //             }
+    //         });
+    //     }
+    //     return () => {
+    //         window.removeEventListener('load', () => {});
+    //     };
+    // }, []);
+    // const alternateStyles = document.querySelectorAll('.alternate-style');
+
+    function setActiveStyle(color: string) {
+        console.log(color);
+
+        // alternateStyles.forEach(style => {
+        //     if (color === style.getAttribute('title')) {
+        //         style.removeAttribute('disabled');
+        //     } else {
+        //         style.setAttribute('disabled', 'true');
+        //     }
+        // });
+    }
+
+    // useEffect(() => {
+    //     window.addEventListener('scroll', () => {
+    //         if (document.querySelector('.style-switcher')?.classList.contains('open')) {
+    //             document.querySelector('.style-switcher')?.classList.remove('open');
+    //         }
+    //     });
+    // }, []);
+    // const styleSwitcherToggle = document.querySelector('.style-switcher-toggler');
+    // styleSwitcherToggle?.addEventListener('click', () => {
+    //     document.querySelector('.style-switcher')?.classList.toggle('open');
+    // });
 
     return (
-        <div className="style-switcher">
-            <div className="style-switcher-toggler s-icon">
-                <i className="fas fa-cog fa-spin"></i>
+        <div className={`style-switcher ${colors ? 'open' : ''}`}>
+            <div className="flex flex-col btns">
+                <div className="flex flex-row mb-2">
+                    <div className="style-switcher-toggler s-icon" onClick={changeColors}>
+                        <i className="fas fa-cog fa-spin"></i>
+                    </div>
+                </div>
+                <div className="flex flex-row">
+                    <div className="day-night s-icon" onClick={changeDark}>
+                        <i className={`fas fa-${dark ? 'sun' : 'moon'}`} />
+                    </div>
+                </div>
             </div>
-            <div className="day-night s-icon">
-                <i className="fas "></i>
-            </div>
-            <h4>Theme Colors</h4>
-            <div className="colors">
-                <span className="color-1" onClick={() => setActiveStyle('color-1')}></span>
-                <span className="color-2" onClick={() => setActiveStyle('color-2')}></span>
-                <span className="color-3" onClick={() => setActiveStyle('color-3')}></span>
-                <span className="color-4" onClick={() => setActiveStyle('color-4')}></span>
-                <span className="color-5" onClick={() => setActiveStyle('color-5')}></span>
+            <div className="flex flex-col">
+                <h4>Theme Colors</h4>
+                <div className={`colors`}>
+                    <span className="color-1" onClick={() => setActiveStyle('color-1')}></span>
+                    <span className="color-2" onClick={() => setActiveStyle('color-2')}></span>
+                    <span className="color-3" onClick={() => setActiveStyle('color-3')}></span>
+                    <span className="color-4" onClick={() => setActiveStyle('color-4')}></span>
+                    <span className="color-5" onClick={() => setActiveStyle('color-5')}></span>
+                </div>
             </div>
         </div>
     );
