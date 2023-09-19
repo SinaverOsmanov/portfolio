393d2b331286dd163a536721e91fce4048b04001
SinaverFrontend <141609479+SinaverFrontend@users.noreply.github.com>
2023-09-20 00:27:31 +0300

    feat:added lazy motion

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,90 +1,92 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { motion } from 'framer-motion';
+import { LazyMotion, domAnimation, m } from 'framer-motion';
 
 export default function Contact() {
     return (
-        <motion.section
-            initial={{ x: '100vw' }}
-            animate={{ x: '0' }}
-            transition={{ duration: 1, bounce: false, type: 'spring', damping: 15, stiffness: 70 }}
-            className="contact section"
-            id="contact"
-        >
-            <div className="container py-16">
-                <div className="row">
-                    <div className="flex-custom-full mb-14 section-title">
-                        <h1>Contact</h1>
+        <LazyMotion features={domAnimation}>
+            <m.section
+                initial={{ x: '100vw' }}
+                animate={{ x: '0' }}
+                transition={{ duration: 1, bounce: false, type: 'spring', damping: 15, stiffness: 70 }}
+                className="contact section"
+                id="contact"
+            >
+                <div className="container py-16">
+                    <div className="row">
+                        <div className="flex-custom-full mb-14 section-title">
+                            <h1>Contact</h1>
+                        </div>
+                    </div>
+                    <h3 className="contact-title">Have You Any Questions ?</h3>
+                    <h4 className="contact-sub-title">I'M AT YOUR SERVICES</h4>
+                    <div className="row flex-wrap">
+                        {/* Contact info item start  */}
+                        <div className="flex-custom-1/4 max-ls:flex-custom-1/2 max-md:flex-custom-full contact-info-item break-words">
+                            <div className="icon">
+                                <i className="fa fa-brands fa-telegram"></i>
+                            </div>
+                            <h4>Write Me On</h4>
+                            <p
+                                className="cursor-pointer hover:underline hover:text-[var(--skin-color)]"
+                                onClick={() => (window.location.href = 'https://t.me/mr_osmanov')}
+                            >
+                                @Mr_Osmanov
+                            </p>
+                        </div>
+                        {/* Contact info item end  */}
+                        {/* Contact info item start  */}
+                        <div className="flex-custom-1/4 max-ls:flex-custom-1/2 max-md:flex-custom-full contact-info-item break-words">
+                            <div className="icon">
+                                <i className="fa fa-map-marker-alt"></i>
+                            </div>
+                            <h4>Address</h4>
+                            <p>Crimea, Evpatoriya</p>
+                        </div>
+                        {/* Contact info item end  */}
+                        {/* Contact info item start  */}
+                        <div className="flex-custom-1/4 max-ls:flex-custom-1/2 max-md:flex-custom-full contact-info-item break-words">
+                            <div className="icon">
+                                <i className="fa fa-envelope"></i>
+                            </div>
+                            <h4>Email</h4>
+                            <p
+                                className="cursor-pointer hover:underline hover:text-[var(--skin-color)]"
+                                onClick={() => (window.location.href = 'https://github.com/SinaverOsmanov')}
+                            >
+                                sinaver.osmanov@gmail.com
+                            </p>
+                        </div>
+                        {/* Contact info item end  */}
+                        {/* Contact info item start  */}
+                        <div className="flex-custom-1/4 max-ls:flex-custom-1/2 max-md:flex-custom-full contact-info-item break-words">
+                            <div className="icon">
+                                <i className="fa fa-globe-asia"></i>
+                            </div>
+                            <h4>GitHub</h4>
+                            <p
+                                className="cursor-pointer hover:underline hover:text-[var(--skin-color)]"
+                                onClick={() => (window.location.href = 'https://github.com/SinaverOsmanov')}
+                            >
+                                SinaverOsmanov
+                            </p>
+                            <p
+                                className="cursor-pointer hover:underline hover:text-[var(--skin-color)]"
+                                onClick={() => (window.location.href = 'https://github.com/SinaverFrontent')}
+                            >
+                                SinaverFrontent
+                            </p>
+                        </div>
+                        {/* Contact info item end  */}
+                    </div>
+                    <h3 className="contact-title">SEND ME AN EMAIL</h3>
+                    <h4 className="contact-sub-title">I'M VERY RESPONSIVE TO MESSAGES</h4>
+                    {/* Contact Form  */}
+                    <div className="row">
+                        <ContactForm />
                     </div>
                 </div>
-                <h3 className="contact-title">Have You Any Questions ?</h3>
-                <h4 className="contact-sub-title">I'M AT YOUR SERVICES</h4>
-                <div className="row flex-wrap">
-                    {/* Contact info item start  */}
-                    <div className="flex-custom-1/4 max-ls:flex-custom-1/2 max-md:flex-custom-full contact-info-item break-words">
-                        <div className="icon">
-                            <i className="fa fa-brands fa-telegram"></i>
-                        </div>
-                        <h4>Write Me On</h4>
-                        <p
-                            className="cursor-pointer hover:underline hover:text-[var(--skin-color)]"
-                            onClick={() => (window.location.href = 'https://t.me/mr_osmanov')}
-                        >
-                            @Mr_Osmanov
-                        </p>
-                    </div>
-                    {/* Contact info item end  */}
-                    {/* Contact info item start  */}
-                    <div className="flex-custom-1/4 max-ls:flex-custom-1/2 max-md:flex-custom-full contact-info-item break-words">
-                        <div className="icon">
-                            <i className="fa fa-map-marker-alt"></i>
-                        </div>
-                        <h4>Address</h4>
-                        <p>Crimea, Evpatoriya</p>
-                    </div>
-                    {/* Contact info item end  */}
-                    {/* Contact info item start  */}
-                    <div className="flex-custom-1/4 max-ls:flex-custom-1/2 max-md:flex-custom-full contact-info-item break-words">
-                        <div className="icon">
-                            <i className="fa fa-envelope"></i>
-                        </div>
-                        <h4>Email</h4>
-                        <p
-                            className="cursor-pointer hover:underline hover:text-[var(--skin-color)]"
-                            onClick={() => (window.location.href = 'https://github.com/SinaverOsmanov')}
-                        >
-                            sinaver.osmanov@gmail.com
-                        </p>
-                    </div>
-                    {/* Contact info item end  */}
-                    {/* Contact info item start  */}
-                    <div className="flex-custom-1/4 max-ls:flex-custom-1/2 max-md:flex-custom-full contact-info-item break-words">
-                        <div className="icon">
-                            <i className="fa fa-globe-asia"></i>
-                        </div>
-                        <h4>GitHub</h4>
-                        <p
-                            className="cursor-pointer hover:underline hover:text-[var(--skin-color)]"
-                            onClick={() => (window.location.href = 'https://github.com/SinaverOsmanov')}
-                        >
-                            SinaverOsmanov
-                        </p>
-                        <p
-                            className="cursor-pointer hover:underline hover:text-[var(--skin-color)]"
-                            onClick={() => (window.location.href = 'https://github.com/SinaverFrontent')}
-                        >
-                            SinaverFrontent
-                        </p>
-                    </div>
-                    {/* Contact info item end  */}
-                </div>
-                <h3 className="contact-title">SEND ME AN EMAIL</h3>
-                <h4 className="contact-sub-title">I'M VERY RESPONSIVE TO MESSAGES</h4>
-                {/* Contact Form  */}
-                <div className="row">
-                    <ContactForm />
-                </div>
-            </div>
-        </motion.section>
+            </m.section>
+        </LazyMotion>
     );
 }
 
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,101 +1,103 @@
-import { motion } from 'framer-motion';
+import { LazyMotion, domAnimation, m } from 'framer-motion';
 
 export default function Services() {
     return (
-        <motion.section
-            initial={{ x: '100vw' }}
-            animate={{ x: '0' }}
-            transition={{ duration: 1, bounce: false, type: 'spring', damping: 15, stiffness: 70 }}
-            className="service section"
-            id="services"
-        >
-            <div className="container py-16">
-                <div className="row">
-                    <div className="basis-full mb-14 section-title">
-                        <h1>Services</h1>
+        <LazyMotion features={domAnimation}>
+            <m.section
+                initial={{ x: '100vw' }}
+                animate={{ x: '0' }}
+                transition={{ duration: 1, bounce: false, type: 'spring', damping: 15, stiffness: 70 }}
+                className="service section"
+                id="services"
+            >
+                <div className="container py-16">
+                    <div className="row">
+                        <div className="basis-full mb-14 section-title">
+                            <h1>Services</h1>
+                        </div>
+                    </div>
+                    <div className="row max-md:flex-wrap justify-between">
+                        {/* Service item Start  */}
+                        <m.div
+                            // initial={{ x: '-100vh' }}
+                            // transition={{
+                            //     delay: 0.1,
+                            //     duration: 1,
+                            //     bounce: true,
+                            //     type: 'spring',
+                            //     damping: 12,
+                            //     stiffness: 100,
+                            // }}
+                            // animate={{ x: 0 }}
+                            className="flex-custom-1/3 max-mg:flex-custom-full service-item mg:pr-3 max-mg:mb-5"
+                        >
+                            <div className="service-item-inner">
+                                <div className="icon">
+                                    <i className="fa fa-mobile-alt"></i>
+                                </div>
+                                <h4>Development Website</h4>
+                                <p>
+                                    I believe it is important to understand both design and development to achieve a
+                                    successful user-facing product.
+                                </p>
+                            </div>
+                        </m.div>
+                        {/* Service item End  */}
+                        {/* Service item Start  */}
+                        <m.div
+                            // initial={{ x: '100vh' }}
+                            // transition={{
+                            //     delay: 0.3,
+                            //     duration: 1,
+                            //     bounce: true,
+                            //     type: 'spring',
+                            //     damping: 12,
+                            //     stiffness: 100,
+                            // }}
+                            // animate={{ x: 0 }}
+                            className="flex-custom-1/3 max-mg:flex-custom-full service-item mg:pr-3 max-mg:mb-5"
+                        >
+                            <div className="service-item-inner">
+                                <div className="icon">
+                                    <i className="fa fa-palette"></i>
+                                </div>
+                                <h4>Landing page</h4>
+                                <p>
+                                    In digital marketing, a landing page is a standalone web page, created specifically
+                                    for a marketing or advertising campaign.
+                                </p>
+                            </div>
+                        </m.div>
+                        {/* Service item End  */}
+                        {/* Service item Start  */}
+                        <m.div
+                            // initial={{ x: '-100vh' }}
+                            // transition={{
+                            //     delay: 0.5,
+                            //     duration: 1,
+                            //     bounce: true,
+                            //     type: 'spring',
+                            //     damping: 12,
+                            //     stiffness: 100,
+                            // }}
+                            // animate={{ x: 0 }}
+                            className="flex-custom-1/3 max-mg:flex-custom-full service-item max-mg:mb-5"
+                        >
+                            <div className="service-item-inner">
+                                <div className="icon">
+                                    <i className="fa fa-code"></i>
+                                </div>
+                                <h4>Web Application</h4>
+                                <p>
+                                    A web application is a computer program that utilizes web browsers and web
+                                    technology to perform tasks over the Internet.
+                                </p>
+                            </div>
+                        </m.div>
+                        {/* Service item End  */}
                     </div>
                 </div>
-                <div className="row max-md:flex-wrap justify-between">
-                    {/* Service item Start  */}
-                    <motion.div
-                        // initial={{ x: '-100vh' }}
-                        // transition={{
-                        //     delay: 0.1,
-                        //     duration: 1,
-                        //     bounce: true,
-                        //     type: 'spring',
-                        //     damping: 12,
-                        //     stiffness: 100,
-                        // }}
-                        // animate={{ x: 0 }}
-                        className="flex-custom-1/3 max-mg:flex-custom-full service-item lg:pr-3 max-mg:mb-5"
-                    >
-                        <div className="service-item-inner">
-                            <div className="icon">
-                                <i className="fa fa-mobile-alt"></i>
-                            </div>
-                            <h4>Development Website</h4>
-                            <p>
-                                I believe it is important to understand both design and development to achieve a
-                                successful user-facing product.
-                            </p>
-                        </div>
-                    </motion.div>
-                    {/* Service item End  */}
-                    {/* Service item Start  */}
-                    <motion.div
-                        // initial={{ x: '100vh' }}
-                        // transition={{
-                        //     delay: 0.3,
-                        //     duration: 1,
-                        //     bounce: true,
-                        //     type: 'spring',
-                        //     damping: 12,
-                        //     stiffness: 100,
-                        // }}
-                        // animate={{ x: 0 }}
-                        className="flex-custom-1/3 max-mg:flex-custom-full service-item lg:pr-3 max-mg:mb-5"
-                    >
-                        <div className="service-item-inner">
-                            <div className="icon">
-                                <i className="fa fa-palette"></i>
-                            </div>
-                            <h4>Landing page</h4>
-                            <p>
-                                In digital marketing, a landing page is a standalone web page, created specifically for
-                                a marketing or advertising campaign.
-                            </p>
-                        </div>
-                    </motion.div>
-                    {/* Service item End  */}
-                    {/* Service item Start  */}
-                    <motion.div
-                        // initial={{ x: '-100vh' }}
-                        // transition={{
-                        //     delay: 0.5,
-                        //     duration: 1,
-                        //     bounce: true,
-                        //     type: 'spring',
-                        //     damping: 12,
-                        //     stiffness: 100,
-                        // }}
-                        // animate={{ x: 0 }}
-                        className="flex-custom-1/3 max-mg:flex-custom-full service-item max-mg:mb-5"
-                    >
-                        <div className="service-item-inner">
-                            <div className="icon">
-                                <i className="fa fa-code"></i>
-                            </div>
-                            <h4>Web Application</h4>
-                            <p>
-                                A web application is a computer program that utilizes web browsers and web technology to
-                                perform tasks over the Internet.
-                            </p>
-                        </div>
-                    </motion.div>
-                    {/* Service item End  */}
-                </div>
-            </div>
-        </motion.section>
+            </m.section>
+        </LazyMotion>
     );
 }
