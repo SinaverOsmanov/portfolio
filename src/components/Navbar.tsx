27e8d566cdbea32952a775343f42135a0dea693b
SinaverFrontend <141609479+SinaverFrontend@users.noreply.github.com>
2023-09-22 13:45:49 +0300

    fix: added rounded-full class to switch btn

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -87,7 +87,7 @@ function LanguagePanel() {
                 value={lang}
                 onChange={handleChange}
                 id="languages"
-                className="w-16 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="w-16 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
                 <option value="en">EN</option>
                 <option value="ru">RU</option>
